426ca75296fe6e93fcd5b9fc4b2b484bde42da39
Jorge Hernández <jorgehg@Jorges-MacBook-Pro.local>
2024-09-07 19:08:31 -0600

    Add logic to the shopping cart

--- src/pages/shopping-cart/ShoppingCart.tsx
+++ src/pages/shopping-cart/ShoppingCart.tsx
@@ -1,11 +1,56 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ShoppingCartContext } from "../../providers/ShoppingCartContext";
 import { ProductCardProps } from "../../components/ProductCard/ProductCard";
 
 import './ShoppingCart.styles.scss';
+import { setToLocalStorage } from "../../utils/localStorage";
+
+const PRODUCT_LIST_KEY = "PRODUCT_LIST_KEY";
 
 const ShoppingCartPage = () => {
-    const { productList } = useContext(ShoppingCartContext);
+    const { productList, setProductList } = useContext(ShoppingCartContext);
+    const [ total, setTotal ] = useState(0);
+
+    useEffect(() => {
+        let totalGlobal = 0;
+        // useEffect Va a estar escuchando cuando las propiedades en el array cambien
+        productList.forEach((product: ProductCardProps) => { // Leemos cada producto de la lista
+            // Calcular el total por producto 
+            const totalByProduct = product.quantity! * product.price;
+
+            // El total por producto lo suma al total general
+            totalGlobal = totalGlobal + totalByProduct;
+        });
+        setTotal(totalGlobal);
+    }, [productList]);
+
+    const handleClick = (id: string) => { // id selecciando
+        // Cuando el usuario de click, se ejecuta esta función
+        // Usamos filter, porque queremos eliminar un elemento de la lista
+        const result = productList.filter((product: ProductCardProps) => {
+            return product.id !== id // Regresamos todos los products que cumplan esta condición
+        });
+        setProductList(result);
+        setToLocalStorage(PRODUCT_LIST_KEY, result);
+    };
+
+    const findProduct = (id: string) => {
+        // Si lo encuentra regresa la posicion, sino regresa un -1
+        const result = productList.findIndex((productSearch: ProductCardProps) => 
+            productSearch.id === id
+        );
+
+        return result;
+    }
+
+    const handleOnQuantityChange = (event: React.ChangeEvent<HTMLInputElement>, id: string) => {
+        const productIndex = findProduct(id); // Buscamos el indice del producto en la lista
+        productList[productIndex].quantity = Number(event.target.value); // Actualizamos la vieja cantidad por la nueva
+        setProductList([...productList]); // Actualizamos la lista de productos con los nuevos datos para
+        // que se vean los datos actualizados en la página
+        setToLocalStorage(PRODUCT_LIST_KEY, [...productList]); // Guardamos en localStorage para que se guarde incluso 
+        // al actualizar
+    }
 
     return <div className="shopping-cart-page">
         <h1>Carrito</h1>
@@ -21,15 +66,24 @@ const ShoppingCartPage = () => {
                                 {product.description}
                             </div>
                             <div>
-                                CANTIDAD: {product.quantity}
+                                CANTIDAD:
+                                <input 
+                                    type="number"
+                                    defaultValue={product.quantity}
+                                    onChange={(e) => {handleOnQuantityChange(e, product.id)}}/>
                             </div>
+                            <button onClick={() => handleClick(product.id)}>Eliminar</button>
                         </div>
                         <div className="shopping-cart-page-product-price">
                             ${product.price}
                         </div>
+                        <div className="shopping-cart-page-product-total">
+                            ${product.quantity! * product.price}
+                        </div>
                     </div>
                 })
             }
+            <div>Total: ${total}</div>
         </div>
     </div>
 };
